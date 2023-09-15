b962bb2cbf84e212ed45ece53f3efb6ca6f1acb4
sushmasrinivasan04@gmail.com <sushmasrinivasan04@gmail.com>
2023-09-15 12:14:10 +0200

    Add project files.

---
+++ Module7Classes/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7classes
+{
+    public class Employee
+    {
+        public string fName;
+        public string lName;
+        public string email;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        public int noOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+        public DateTime birthDay;
+
+        public Employee(string v1, string v2, string v3, DateTime dateTime, int v4)
+        {
+        }
+
+        public void PerformWork()
+        {
+            // noOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{fName} {lName} has worked for {noOfHoursWorked} hours");
+
+        }
+        public void PerformWork(int noOfHoursWorked)
+        {
+            noOfHoursWorked += noOfHoursWorked;
+            Console.WriteLine($"{fName} {lName} has worked for {noOfHoursWorked} hours");
+        }
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = noOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{fName} {lName} has received a wage of {wage} for {noOfHoursWorked} hour(s) of work.");
+
+            if (resetHours)
+                noOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{fName}\nLast name: \t{lName}\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+        
+
+
+    }
+}
---
+++ Module7Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Module7classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new("Sushma", "Srini", "Sush@123.com", new DateTime(1994, 03, 15), 25);
+            e.DisplayEmployeeDetails();
+            e.PerformWork(2);
+            e.PerformWork(8);
+            
+
+            double receiveWage=e.ReceiveWage(true);
+            Console.WriteLine($"Wage paid (message from program): {receiveWage}");
+        }
+    }
+}
